cea47c4f22fb5fa4995200643ff74afaf64bf360
Andrew Jarvi <andrewjarvi9@gmail.com>
2023-04-11 10:03:36 -0400

    movie card started

--- vue/src/components/MovieCard.vue
+++ vue/src/components/MovieCard.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <div class="card">
+    <div class="card-image">
+      <img v-bind:src="movie.poster" alt="" />
+    </div>
+    <div class="card-content">
+      <h4>{{ movie.title }}</h4>
+      <button>Like</button>
+      <button>Disklike</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["movie"],
+};
 </script>
 
-<style>
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+}
+.card-image {
+  height: 2rem;
+  width: 80%;
+}
+.card-content {
+  flex: 1;
+}
 </style>
