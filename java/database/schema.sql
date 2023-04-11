8447d483ea21678107739306f1e0bd13a0c8f3c0
Amy Edwards <amy.m.edwards1990@gmail.com>
2023-04-11 12:01:57 -0400

    data set up finished

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS movie_genre, genre, movies, users;
+
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -12,11 +12,27 @@ CREATE TABLE users (
 );
 
 create table movies (
-	movie_id INT,
-	title VARCHAR(50),
-	release_date DATE,
-	genre VARCHAR(50),
-	poster VARCHAR(50)
+  	id INT PRIMARY KEY,
+  	title VARCHAR(50),
+  	release_date DATE,
+  	poster VARCHAR(50),
+  	overview TEXT
+  );
+
+create table genre (
+    genre_id INT PRIMARY KEY,
+    genre_name VARCHAR(100)
 );
+--
+create table movie_genre (
+    movie_id INT,
+    genre_id INT,
+    	CONSTRAINT PK_movie_genre PRIMARY KEY(movie_id, genre_id),
+        CONSTRAINT FK_movie_genre_movie FOREIGN KEY(movie_id) REFERENCES movies(id),
+        CONSTRAINT FK_movie_genre_genre FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
+);
+--
+
+
 
 COMMIT TRANSACTION;
