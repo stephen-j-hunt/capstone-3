ef36fef3ddc8626565eac34423090924c495d257
Stephen <stephen.hunt126@gmail.com>
2023-04-20 12:45:57 -0400

    final sync before moving to github acc

---
+++ vue/src/components/LoadingIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loading-icon">
+    <i class="fa fa-spinner fa-spin fa-3x"></i>
+  </div>
+</template>
+
+<style>
+.loading-icon {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 999;
+  color: #333;
+}
+</style>
